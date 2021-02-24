db8341bef628bfe8ea6e40db8e648bbca338f4dd
Kevin Smith <futuretense@gmail.com>
2021-02-23 18:56:50 -0800

    Updated using ternary operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import Login from "./Login";
 
+var isLoggedIn = false;
+
+const currentTime = new Date().getHours();
+console.log(currentTime);
+// replaced by ternary operator
+// function renderConditionally() {
+//   if (isLoggedIn === true) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Login />;
+//   }
+// }
+//using ternary operator in App(). React requires
+//expressions and not conditons. able to get logic of conditonal statement
+//but expressed like an expression through ternary operator
+//condition ? do if true : do if false
+// below askes-- isLoggedIn true if yes display hello if not display Login component
 function App() {
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
   );
 }
 
